40ac37dfb7e5c72cb0fc647d314bb721f7632f0d
xi-xo <jose_aguiar@hotmail.com.uk>
2025-04-15 10:30:38 +0100

    working on js file

---
+++ test.js
@@ -0,0 +1,9 @@
+//global variable use with
+const multiplier = 2;
+// console.log(multiplier * 2.37); 
+
+//function and use the multiplier
+function multiply(num) {
+return num * multiplier; 
+}
+console.log(multiply(5))
